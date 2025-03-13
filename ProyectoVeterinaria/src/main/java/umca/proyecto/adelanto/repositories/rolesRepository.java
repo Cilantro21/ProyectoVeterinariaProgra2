9d194250eeea56310783b7b2c281e7753c4e3df0
Cilantro21 <aleamundo@hotmail.com>
2025-03-12 19:30:21 -0600

    Cambios realizados en usuarioController, vista y generado carpeta de autenticador con archivo

--- ProyectoVeterinaria/src/main/java/umca/proyecto/adelanto/repositories/rolesRepository.java
+++ ProyectoVeterinaria/src/main/java/umca/proyecto/adelanto/repositories/rolesRepository.java
@@ -3,7 +3,10 @@ package umca.proyecto.adelanto.repositories;
 import org.springframework.data.repository.CrudRepository;
 
 import umca.proyecto.adelanto.entities.rolesEntity;
+import umca.proyecto.adelanto.entities.usuariosEntity;
 
 public interface rolesRepository extends CrudRepository<rolesEntity, Integer>{
+
+    usuariosEntity findByUsuario(String usuario);
 
 }
